e68303fe92e3f09fc2d86254c1947cfa47473189
David Jelenc <david.jelenc@fri.uni-lj.si>
2018-10-10 13:48:09 +0200

    Adds starter application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import atb.app.gui.AppKt;
+
+public class App {
+    public static void main(String[] args) {
+        // starts ATB GUI interface
+        AppKt.main(args);
+    }
+}
